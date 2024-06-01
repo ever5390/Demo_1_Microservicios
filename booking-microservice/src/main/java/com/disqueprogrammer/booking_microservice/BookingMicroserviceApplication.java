50f906be732ab9635eed9adcf2e4d2f34de8df47
Ever Rosales <everjrosalesp@gmail.com>
2024-06-01 09:41:33 -0500

    Se configura el producto MS para comunicarse con CONFIG-SERVER y obtener sus coniguraciones

--- booking-microservice/src/main/java/com/disqueprogrammer/booking_microservice/BookingMicroserviceApplication.java
+++ booking-microservice/src/main/java/com/disqueprogrammer/booking_microservice/BookingMicroserviceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class BookingMicroserviceApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookingMicroserviceApplication.class, args);
 	}
