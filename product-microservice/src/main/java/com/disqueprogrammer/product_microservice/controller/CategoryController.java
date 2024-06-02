0305e5e8fb857fd4d69e31813050fe89934fd836
Ever Rosales <everjrosalesp@gmail.com>
2024-06-01 23:57:34 -0500

    Se configura el MS Booking con bootstrap dependy al igual que produc y se configura MS PRoduct con actuator para aplicar el refresh Scope

--- product-microservice/src/main/java/com/disqueprogrammer/product_microservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/disqueprogrammer/product_microservice/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.disqueprogrammer.product_microservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Getter
 @Setter
 @RequestMapping("api/categories")
+@RefreshScope
 public class CategoryController {
     @Value("${app.testProperty}")
     private String testProperty;
 
+
+    @GetMapping("/test_property")
     private String getTestProperty() {
         return this.testProperty;
     }
